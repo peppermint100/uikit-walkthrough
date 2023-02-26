526e9fb32f86c3665d59575387871e7e48509a40
peppermint100 <krpeppermint100@gmail.com>
2023-02-26 10:56:34 +0900

    ui design for resultvc

--- inflearn-allen-course/BMIApp/BMIApp/ResultViewController.swift
+++ inflearn-allen-course/BMIApp/BMIApp/ResultViewController.swift
@@ -9,12 +9,59 @@ import UIKit
 
 class ResultViewController: UIViewController {
     
+    private let textSize: CGFloat = 18
+    
+    private lazy var resultLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: textSize)
+        label.text = "BMI 결과값"
+        label.textAlignment = .center
+        return label
+    }()
+    
+    private lazy var bmiResultLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.boldSystemFont(ofSize: textSize + 4)
+        label.textAlignment = .center
+        label.backgroundColor = .systemPink
+        label.layer.cornerRadius = 5
+        label.clipsToBounds = true
+        label.text = "26.12"
+        return label
+    }()
+    
+    private lazy var adviceLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: textSize)
+        label.textAlignment = .center
+        label.text = "살을 빼세요"
+        return label
+    }()
+    
     private lazy var stackView: UIStackView = {
-        let sv = UIStackView()
-        sv.backgroundColor = .systemBlue
+        let sv = UIStackView(arrangedSubviews: [resultLabel, bmiResultLabel, adviceLabel])
+        sv.axis = .vertical
+        sv.spacing = 15
+        sv.distribution = .fill
         return sv
     }()
-
+    
+    private lazy var backButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("다시 계산하기", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.layer.cornerRadius = 5
+        button.clipsToBounds = true
+        button.backgroundColor = .systemBlue
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: textSize)
+        button.addTarget(self, action: #selector(backButtonTapped), for: .touchUpInside)
+        return button
+    }()
+    
+    @objc func backButtonTapped() {
+        dismiss(animated: true)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -25,16 +72,36 @@ class ResultViewController: UIViewController {
     
     func buildUI() {
         view.addSubview(stackView)
+        view.addSubview(backButton)
         view.backgroundColor = .lightGray
     }
     
     func setAutolayout() {
         stackView.translatesAutoresizingMaskIntoConstraints = false
+        bmiResultLabel.translatesAutoresizingMaskIntoConstraints = false
+        resultLabel.translatesAutoresizingMaskIntoConstraints = false
+        adviceLabel.translatesAutoresizingMaskIntoConstraints = false
+        backButton.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 130),
+            stackView.widthAnchor.constraint(equalToConstant: 200),
             stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            stackView.topAnchor.constraint(equalTo: view.topAnchor, constant: 130),
-            stackView.heightAnchor.constraint(equalToConstant: 160)
+            
+            resultLabel.leadingAnchor.constraint(equalTo: stackView.leadingAnchor, constant: 0),
+            resultLabel.trailingAnchor.constraint(equalTo: stackView.trailingAnchor, constant: 0),
+             
+            bmiResultLabel.leadingAnchor.constraint(equalTo: stackView.leadingAnchor, constant: 8),
+            bmiResultLabel.trailingAnchor.constraint(equalTo: stackView.trailingAnchor, constant: -8),
+            bmiResultLabel.heightAnchor.constraint(equalToConstant: 50),
+           
+            adviceLabel.leadingAnchor.constraint(equalTo: stackView.leadingAnchor, constant: 0),
+            adviceLabel.trailingAnchor.constraint(equalTo: stackView.trailingAnchor, constant: 0),
+            
+            backButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            backButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            backButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -30),
+            backButton.heightAnchor.constraint(equalToConstant: 45)
         ])
     }
 }
