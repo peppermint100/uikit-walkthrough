2b71201b0fa4cbb09db3c1171c885753c1976cc4
peppermint100 <krpeppermint100@gmail.com>
2023-07-29 15:34:12 +0900

    SpotifyClone #5

--- SpotifyClone/SpotifyClone/Managers/AuthManager.swift
+++ SpotifyClone/SpotifyClone/Managers/AuthManager.swift
@@ -127,13 +127,13 @@ final class AuthManager {
     }
     
     
-    public func refreshIfNeeded(completion: @escaping (Bool) -> Void) {
+    public func refreshIfNeeded(completion: ((Bool) -> Void)?) {
         guard !refreshingToken else {
             return
         }
         
         guard shouldRefreshToken else {
-            completion(true)
+            completion?(true)
             return
         }
         
@@ -159,7 +159,7 @@ final class AuthManager {
         let basicToken = Constants.clientID + ":" + Constants.clientSecret
         let data = basicToken.data(using: .utf8)
         guard let base64String = data?.base64EncodedString() else {
-            completion(false)
+            completion?(false)
             return
         }
         request.setValue("Basic \(base64String)", forHTTPHeaderField: "Authorization")
@@ -167,7 +167,7 @@ final class AuthManager {
         let task = URLSession.shared.dataTask(with: request) {[weak self] data, _, error in
             self?.refreshingToken = false
             guard let data = data, error == nil else {
-                completion(false)
+                completion?(false)
                 return
             }
             
@@ -177,9 +177,9 @@ final class AuthManager {
                 self?.onRefreshBlocks.forEach { $0(result.access_token) }
                 self?.onRefreshBlocks.removeAll()
                 self?.cacheToken(result: result)
-                completion(true)
+                completion?(true)
             } catch {
-                completion(false)
+                completion?(false)
             }
         }
         
