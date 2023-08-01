edc1c804602b1710c963d76afa8b82a0161ec322
peppermint100 <krpeppermint100@gmail.com>
2023-08-01 22:38:52 +0900

    SpotifyClone #12 50%

---
+++ SpotifyClone/SpotifyClone/View/PlaylistHeaderCollectionReusableView.swift
@@ -0,0 +1,76 @@
+//
+//  PlaylistHeaderCollectionReusableView.swift
+//  SpotifyClone
+//
+//  Created by peppermint100 on 2023/08/01.
+//
+
+import SDWebImage
+import UIKit
+
+final class PlaylistHeaderCollectionReusableView: UICollectionReusableView {
+    static let identifier = "PlaylistHeaderCollectionReusableView"
+    
+    private let nameLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 22, weight: .semibold)
+        return label
+    }()
+    
+    private let descriptionLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 18, weight: .regular)
+        label.numberOfLines = 0
+        label.textColor = .secondaryLabel
+        return label
+    }()
+    
+    private let ownerLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 18, weight: .light)
+        label.textColor = .secondaryLabel
+        return label
+    }()
+    
+    private let imageView: UIImageView = {
+        let iv = UIImageView()
+        iv.contentMode = .scaleAspectFill
+        iv.image = UIImage(systemName: "photo")
+        return iv
+    }()
+    
+    private let playAllButton: UIButton = {
+        let button = UIButton
+        return button
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .systemBackground
+        addSubview(imageView)
+        addSubview(nameLabel)
+        addSubview(descriptionLabel)
+        addSubview(ownerLabel)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        let imageSize: CGFloat = height / 1.8
+        imageView.frame = CGRect(x: (width - imageSize)/2, y: 20, width: imageSize, height: imageSize)
+        
+        nameLabel.frame = CGRect(x: 10, y: imageView.bottom, width: width-20, height: 44)
+        descriptionLabel.frame = CGRect(x: 10, y: nameLabel.bottom, width: width-20, height: 44)
+        ownerLabel.frame = CGRect(x: 10, y: descriptionLabel.bottom, width: width-20, height: 44)
+    }
+    
+    func configure(with viewModel: PlaylistHeaderViewViewModel) {
+        nameLabel.text = viewModel.name
+        ownerLabel.text = viewModel.ownerName
+        descriptionLabel.text = viewModel.description
+        imageView.sd_setImage(with: viewModel.artworkURL, completed: nil)
+    }
+}
---
+++ SpotifyClone/SpotifyClone/View/PlaylistHeaderViewViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  PlaylistHeaderViewViewModel.swift
+//  SpotifyClone
+//
+//  Created by peppermint100 on 2023/08/01.
+//
+
+import Foundation
+
+struct PlaylistHeaderViewViewModel {
+    let name: String?
+    let ownerName: String?
+    let description: String?
+    let artworkURL: URL?
+}
