49198431c0775f7d7acfe84222265daa468bc360
peppermint100 <krpeppermint100@gmail.com>
2023-08-15 20:26:56 +0900

    SpotifyClone #22 - createPlaylist

--- SpotifyClone/SpotifyClone/Controllers/Other/Library/LibraryPlaylistsViewController.swift
+++ SpotifyClone/SpotifyClone/Controllers/Other/Library/LibraryPlaylistsViewController.swift
@@ -8,21 +8,82 @@
 import UIKit
 
 class LibraryPlaylistsViewController: UIViewController {
+    
+    var playlists = [Playlist]()
+    
+    private let noPlaylistsView = ActionLabelView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemPink
+        view.backgroundColor = .systemBackground
+        setUpNoPlaylistView()
+        fetchData()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        noPlaylistsView.frame = CGRect(x: 0, y: 0, width: 150, height: 150)
+        noPlaylistsView.center = view.center
     }
-    */
+    
+    private func updateUI() {
+        if playlists.isEmpty {
+            noPlaylistsView.isHidden = false
+        }
+        else {
+        }
+    }
+    
+    private func setUpNoPlaylistView() {
+        view.addSubview(noPlaylistsView)
+        noPlaylistsView.delegate = self
+        noPlaylistsView.configure(with: ActionLabelViewViewModel(text: "You Don't Have Any Playlists Yet", actionTitle: "Create"))
+    }
+    
+    private func fetchData() {
+        APICaller.shared.getCurrentUserPlaylists { [weak self] result in
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let playlists):
+                    self?.playlists = playlists
+                    self?.updateUI()
+                case .failure(let error):
+                    print(error.localizedDescription)
+                }
+            }
+        }
+    }
+}
 
+extension LibraryPlaylistsViewController: ActionLabelViewDelegate {
+    func actionLabelViewDidTapButton(_ actionView: ActionLabelView) {
+        let alert = UIAlertController(
+            title: "New Playlists",
+            message: "Enter Playlist name",
+            preferredStyle: .alert)
+        
+        alert.addTextField { textField in
+            textField.placeholder = "Playlist..."
+        }
+        
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        alert.addAction(UIAlertAction(title: "Create", style: .default) { _ in
+            guard let field = alert.textFields?.first,
+                  let text = field.text,
+                  !text.trimmingCharacters(in: .whitespaces).isEmpty else {
+                return
+            }
+            
+            print(text)
+            
+            APICaller.shared.createPlaylist(with: text) { success in
+                if success {
+                    
+                }
+                else {
+                    print("Failed to create playlist")
+                }
+            }})
+        present(alert, animated: true)
+    }
 }
---
+++ SpotifyClone/SpotifyClone/Models/LibraryPlaylistsResponse.swift
@@ -0,0 +1,13 @@
+//
+//  LibraryPlaylistsResponse.swift
+//  SpotifyClone
+//
+//  Created by peppermint100 on 2023/08/15.
+//
+
+import Foundation
+                    
+
+struct LibraryPlaylistsResponse: Codable {
+    let items: [Playlist]
+}
