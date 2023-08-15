c2275579451eca7bc38dff642ad55c325132d0e5
peppermint100 <krpeppermint100@gmail.com>
2023-08-15 17:27:26 +0900

    SpotifyClone #21

---
+++ SpotifyClone/SpotifyClone/Controllers/Other/Library/LibraryAlbumsViewController.swift
@@ -0,0 +1,16 @@
+//
+//  LibraryAlbumsViewController.swift
+//  SpotifyClone
+//
+//  Created by peppermint100 on 2023/08/15.
+//
+
+import UIKit
+
+class LibraryAlbumsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+    }
+}
---
+++ SpotifyClone/SpotifyClone/Controllers/Other/Library/LibraryPlaylistsViewController.swift
@@ -0,0 +1,28 @@
+//
+//  LibraryPlaylistsViewController.swift
+//  SpotifyClone
+//
+//  Created by peppermint100 on 2023/08/15.
+//
+
+import UIKit
+
+class LibraryPlaylistsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
